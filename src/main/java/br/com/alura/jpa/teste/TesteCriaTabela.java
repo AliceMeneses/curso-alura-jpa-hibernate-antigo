72cbd5448b734d921219445f27a621866c9d58d7
Alice <am171696@alunos.unisanta.br>
2020-12-31 14:35:44 -0300

    Fechando o entityManager

--- src/main/java/br/com/alura/jpa/teste/TesteCriaTabela.java
+++ src/main/java/br/com/alura/jpa/teste/TesteCriaTabela.java
@@ -1,10 +1,15 @@
 package br.com.alura.jpa.teste;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class TesteCriaTabela {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("contas");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.close();
 	}
 
 }

da515819269eacdcab25c1808ad1d4e12088424e
Alice <am171696@alunos.unisanta.br>
2021-01-01 19:18:58 -0300

    Buscando todas as movimentacoes da conta da Alice

---
+++ src/main/java/br/com/alura/jpa/teste/TesteJPQL.java
@@ -0,0 +1,38 @@
+package br.com.alura.jpa.teste;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import br.com.alura.jpa.modelo.Conta;
+import br.com.alura.jpa.modelo.MovimentacaoBancaria;
+
+public class TesteJPQL {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("contas");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+		Conta conta = new Conta();
+		conta.setId(1L);
+
+		String jpql = "Select m from MovimentacaoBancaria m where m.conta = :pConta";
+
+		Query query = entityManager.createQuery(jpql);
+		query.setParameter("pConta", conta);
+		List<MovimentacaoBancaria> movimentacoes = query.getResultList();
+
+		for (MovimentacaoBancaria movimentacao : movimentacoes) {
+			System.out.println("Descrição: " + movimentacao.getDescricao());
+			System.out.println("Tipo: " + movimentacao.getTipoMovimentaoBancaria());
+			System.out.println("Valor: " + movimentacao.getValor());
+		}
+		
+		entityManager.close();
+	}
+
+}

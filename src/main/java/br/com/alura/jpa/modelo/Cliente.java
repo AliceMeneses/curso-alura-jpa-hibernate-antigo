83fd228a66acdfb1249358c47696d67796458380
Alice <am171696@alunos.unisanta.br>
2021-01-01 19:17:57 -0300

    Adicionando anotacao JoinColum para existir somente uma conta para um cliente e vice versa

--- src/main/java/br/com/alura/jpa/modelo/Cliente.java
+++ src/main/java/br/com/alura/jpa/modelo/Cliente.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -16,7 +17,8 @@ public class Cliente {
 	private String nome;
 	private String profissao;
 	private String endereco;
-	
+
+	@JoinColumn(unique = true)
 	@OneToOne
 	private Conta conta;
 
@@ -50,5 +52,13 @@ public class Cliente {
 
 	public void setEndereco(String endereco) {
 		this.endereco = endereco;
+	}
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
 	}
 }

ad2ff38cb04f66fbe544cdbad298f15e96a5fda7
Alice <am171696@alunos.unisanta.br>
2021-01-03 10:08:18 -0300

    Gerando relatorio com todas as contas e todas as suas movimentacoes

---
+++ src/main/java/br/com/alura/jpa/teste/TesteRelatorioDasMovimentacoes.java
@@ -0,0 +1,32 @@
+package br.com.alura.jpa.teste;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import br.com.alura.jpa.modelo.Conta;
+
+public class TesteRelatorioDasMovimentacoes {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("contas");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		String jpql = "Select c from Conta c";
+		TypedQuery<Conta> query = entityManager.createQuery(jpql, Conta.class);
+		List<Conta> contas = query.getResultList();
+		
+		for(Conta conta : contas) {
+			System.out.println("Titular: " + conta.getTitular());
+			System.out.println("Agencia" + conta.getAgencia());
+			System.out.println("Número: " + conta.getNumero());
+			System.out.println("Movimentações: " + conta.getMovimentacoes());
+		}
+		
+	}
+
+}
